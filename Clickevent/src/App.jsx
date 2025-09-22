790a2c37bc366711015f6c9307321128385f29d9
Amol93872 <amolpatelpaik444@gmail.com>
2025-09-22 19:54:21 +0530

    deleted

--- Clickevent/src/App.jsx
+++ Clickevent/src/App.jsx
@@ -4,4 +4,5 @@ function App() {
   return <Button></Button>;
 }
 export default App;
+
 // click events = an interaction when user click on a specific elements we can respond to clicks by passing a callback to the onclick event handler.
